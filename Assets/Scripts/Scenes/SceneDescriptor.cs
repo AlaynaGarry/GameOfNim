6845564f2a6f9a12b6bbc8c73cb12d1578b2125b
Gus <gusmcg1856@gmail.com>
2022-04-08 14:22:28 -0600

    Added Setup Files

---
+++ Assets/Scripts/Scenes/SceneDescriptor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneDescriptor : MonoBehaviour
+{
+    public GameObject player;
+    public Transform playerSpawn;
+    public AudioClip music;
+
+    private void Start()
+    {
+        
+    }
+}
