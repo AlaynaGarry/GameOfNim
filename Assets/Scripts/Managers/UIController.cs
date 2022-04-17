e7a4460e9e7c23f35d5878afb0b604d7b8d1b5c3
LoganBailey890 <lbailey@student.neumont.edu>
2022-04-17 15:45:03 -0600

    4/17/2022 force commite to pull

--- Assets/Scripts/Managers/UIController.cs
+++ Assets/Scripts/Managers/UIController.cs
@@ -6,6 +6,13 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] GameObject activeMenu;
 
+    public string tempDifficaly;
+
+    public string tempName1;
+    public string tempName2;
+
+
+
     public void OnStartScene(string sceneName)
     {
 
@@ -23,5 +30,21 @@ public class UIController : MonoBehaviour
         menu.SetActive(true);
         activeMenu = menu;
     }
+
+    public void setTempDif(string dif)
+    {
+        tempDifficaly = dif;
+        Debug.Log(tempDifficaly);
+    }
+
+    public void setTempName1(string name)
+    {
+        tempName1 = name;
+    }
+    public void setTempName2(string name)
+    {
+        tempName2 = name;
+    }
+
 
 }
