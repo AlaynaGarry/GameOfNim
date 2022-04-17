8e2af24f56b95e4f3275b766a9972f59fe292fe9
Alexgreano <alexisggr104@gmail.com>
2022-04-17 12:57:19 -0600

    4/17/22 - piece mechanic working

---
+++ Assets/Scripts/GameScripts/Tree.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    [SerializeField] GameObject gamePiece;
+    [SerializeField] GameObject Board;
+    [SerializeField] GameObject RowForBoard;
+
+    public int rows;
+    public int TopRowCount = 3;
+    public static int? activeRow;
+
+    static Dictionary<int, List<GameObject>> board = new Dictionary<int, List<GameObject>>();
+    
+    public void RemovePiece(GameObject piece)
+    {
+        if(activeRow == null)
+        {
+            activeRow = piece.GetComponent<GamePiece>().Row;
+        }
+
+        int row = piece.GetComponent<GamePiece>().Row;
+        if(activeRow == row)
+        {
+            board[row].Remove(piece);
+            if(board[row].Count == 0)
+            {
+                Destroy(piece.transform.parent.gameObject);
+            }
+            else
+            {
+                Destroy(piece);
+            }
+        }
+    }
+
+    public void PopulateTree()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            GameObject row = Instantiate(RowForBoard, Board.transform);
+            List<GameObject> newList = new List<GameObject> { };
+            for (int j = 0; j < TopRowCount + (2 * i); j++)
+            {
+                GameObject go = Instantiate(gamePiece, row.transform);
+                go.GetComponent<GamePiece>().Row = i;
+                newList.Add(go);
+            }
+            board.Add(i, newList);
+        }   
+    }
+
+
+
+}
